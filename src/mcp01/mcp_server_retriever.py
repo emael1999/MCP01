b694c5443fd78b014c9c3fb285ffc06cbf42eba7
emael1999 <mal1999@tlen.pl>
2025-10-24 21:27:05 +0200

    No dużo by pisać. W każdym razie rozwijam w kierunku uniwesalnego serwera chunków.

--- src/mcp01/mcp_server_retriever.py
+++ src/mcp01/mcp_server_retriever.py
@@ -8,6 +8,10 @@ import json
 #import asyncio
 #from fastmcp import Client 
 
+from ragcommons.embdbutils import EmbDBTypes
+from ragcommons.embfunc import EmbeddingFunctionTypes
+from ragcommons.embdbutils import getEmbDBHandle
+
 def load_config(path="config.json") -> dict:
     with open(path, "r") as f:
         return json.load(f)
@@ -15,20 +19,72 @@ def load_config(path="config.json") -> dict:
 CONFIG_PATH = os.getenv("MCP_CONFIG_PATH", "config.json")
 config = load_config(CONFIG_PATH)
 
-TOOL_DESCRIPTION = config.get("description", "Default tool description.")
-TOOL_NAME = config.get("tool_name", "retriever_chunks")
-TOOL_TAGS = config.get("tool_tags", ["retrieval", "search", "context", "llm"])
-DB_HOST = config.get("db_host", "localhost")  # <- This is your new config parameter
-
-REQUIRED_KEYS = ["description", "tool_name", "tool_tags", "db_host"]
+REQUIRED_KEYS = ["server_name", 
+                 "tool_description", "tool_name", "tool_tags", 
+                 "db_type", "db_host", "db_port", "db_collection",
+                 "embedding_function_type","embedding_function_address"]
 for key in REQUIRED_KEYS:
     if key not in config:
         raise ValueError(f"Missing required config key: {key}")
     else:
         print(f"[CONFIG] {key}: {config.get(key)}")
 
+SERVER_NAME = config.get("server_name", "Retriever Tools MCP Server")
+TOOL_DESCRIPTION = config.get("tool_description", "Default tool description")
+TOOL_NAME = config.get("tool_name", "retriever_chunks")
+TOOL_TAGS = config.get("tool_tags", ["retrieval", "search", "context", "llm"])
 
-mcp_retriever = FastMCP(name="Retriever Tools MCP Server")
+mcp_retriever = FastMCP(name=SERVER_NAME)
+   
+
+class MCPRetrieverConfig:
+    def __init__(self, 
+                db_collection : str,
+                embedding_function_address : str,                 
+                db_type : EmbDBTypes = EmbDBTypes.chroma,
+                db_host : str ='localhost', 
+                db_port : int = 8000, 
+                embedding_function_type : EmbeddingFunctionTypes 
+                                = EmbeddingFunctionTypes.chroma_default
+    ):
+        self.db_type = db_type
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_collection = db_collection
+        self.embedding_function_type = embedding_function_type        
+        self.embedding_function_address = embedding_function_address
+
+
+
+class MCPRetriever:
+    def __init__(self, config : MCPRetrieverConfig):
+        print(f'Inicjujemy {self.__class__.__name__} z parametrami:')
+        for conf_field_name, conf_field_value in vars(config).items():
+            print(f"{conf_field_name} = {conf_field_value}")
+        self.config = None
+        self.retriever = None
+        self.config = config
+        self.initRetriever(config)
+
+    def initRetriever(self, config : MCPRetrieverConfig):
+        print("initRetriever start")
+        _, _, vector_store = getEmbDBHandle(config.db_type, 
+                                            config.db_host, 
+                                            config.db_port, 
+                                            config.db_collection, 
+                                            config.embedding_function_type, 
+                                            config.embedding_function_address)
+        self.retriever = vector_store.as_retriever()
+
+
+retrieveConfig = MCPRetrieverConfig(db_type = EmbDBTypes(config.get("db_type", EmbDBTypes.chroma.value)),
+                                    db_host = config.get("db_host", 'localhost'), 
+                                    db_port = config.get("db_port", 8000), 
+                                    db_collection = config.get("db_collection"),
+                                    embedding_function_type = EmbeddingFunctionTypes(config.get("embedding_function_type", EmbeddingFunctionTypes.chroma_default.value)),
+                                    embedding_function_address = config.get("embedding_function_address"))
+
+retriever = MCPRetriever(retrieveConfig)
 
 @mcp_retriever.tool(
     name=TOOL_NAME,
